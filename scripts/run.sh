3b9b05d1a0379c0a354170ed1a7cd829dc944c94
k0tk267 <k0tk2670@gmail.com>
2023-02-10 23:40:14 +0900

    feat: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,16 +2,22 @@ source .env
 
 input_file_name=$1
 output_file_name=$2
-batch_size=$3
-epoch_size=$4
+message=${output_file_name%.*}
 time_stamp=$(date "+%Y-%m-%dT%H:%M:%S")
 
 command="
     poetry run papermill \
     ${PROJECT_PATH}/src/notebooks/$input_file_name \
-    ${PROJECT_PATH}/data/results/notebooks/${time_stamp}_$output_file_name \
-    -p BATCH_SIZE $batch_size \
-    -p EPOCH $epoch_size \
+    ${PROJECT_PATH}/results/notebooks/${time_stamp}_$output_file_name \
+    -p MAX_TOKEN_LENGTH 512 \
+    -p BATCH_SIZE_TRAIN 32 \
+    -p BATCH_SIZE_VALID 32 \
+    -p BATCH_SIZE_TEST 32 \
+    -p EPOCHS 10000 \
+    -p PATIENCE 100 \
+    -p GPU_NUM 1 \
+    -p TIME_STAMP $time_stamp \
+    -p MESSAGE $message \
     --request-save-on-cell-execute \
     --prepare-execute \
     --log-output \
